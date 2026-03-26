5db04aea43d4e8a39d8f8556cf66ddb56e7738b5
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-03-26 18:43:26 +0545

    Add files via upload

---
+++ b.py
@@ -0,0 +1,19 @@
+# Matrix Multiplication
+A = [[1, 2],
+     [3, 4]]
+
+B = [[5, 6],
+     [7, 8]]
+
+# Result matrix initialization
+result = [[0, 0],
+          [0, 0]]
+
+for i in range(len(A)):
+    for j in range(len(B[0])):
+        for k in range(len(B)):
+            result[i][j] += A[i][k] * B[k][j]
+
+print("Matrix A x B =")
+for row in result:
+    print(row)
