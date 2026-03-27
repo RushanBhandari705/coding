bdf16de0feb6a0ada6e39cb4ee520c9a61898179
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-03-27 21:08:31 +0545

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Student {
+    private String name = "Rushan";
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        System.out.println(s.getName());
+    }
+}
