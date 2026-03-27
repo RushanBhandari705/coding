89b04a6dfe0cf790909ecbb872cdbee0f5b1c291
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-03-27 21:09:58 +0545

    Add files via upload

---
+++ k.py
@@ -0,0 +1,11 @@
+scores = {"Rushan": 85, "Aayush": 92, "Bikash": 78}
+
+# By value
+sorted_by_value = dict(sorted(scores.items(), key=lambda x: x[1]))
+print(sorted_by_value)  
+# {'Bikash': 78, 'Rushan': 85, 'Aayush': 92}
+
+# By key
+sorted_by_key = dict(sorted(scores.items(), key=lambda x: x[0]))
+print(sorted_by_key)  
+# {'Aayush': 92, 'Bikash': 78, 'Rushan': 85}
