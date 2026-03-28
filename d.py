9f8a555b7822aaca1a8a554bcd640ff06f9146f2
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-03-28 11:46:48 +0545

    Change array values and update print statement

--- d.py
+++ d.py
@@ -1,8 +1,8 @@
-arr = [64, 34, 25, 12, 22, 11, 90]
+arr = [15,78,59,09,45,65]
 
 for i in range(len(arr)):
     for j in range(0, len(arr)-i-1):
         if arr[j] > arr[j+1]:
             arr[j], arr[j+1] = arr[j+1], arr[j]
 
-print("Sorted array:", arr)
+print("The array in sorted order is :", arr)
