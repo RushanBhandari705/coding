0df8f69499c7f3ed795f88302a70d9ff2c6f0b94
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-03-28 11:51:39 +0545

    Change initial number for reversal from 1234 to 6789

--- Reverse.java
+++ Reverse.java
@@ -1,6 +1,6 @@
 public class Reverse {
     public static void main(String[] args) {
-        int num = 1234, rev = 0;
+        int num = 6789, rev = 0;
 
         while(num > 0) {
             int digit = num % 10;
