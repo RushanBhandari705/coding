7a25c063a139ddb9b8b5732c98ef9b4ea3170265
Rushan Bhandari <rushanbhandari3@gmail.com>
2026-03-28 11:48:41 +0545

    Update Aayush's score from 92 to 82

--- k.py
+++ k.py
@@ -1,4 +1,4 @@
-scores = {"Rushan": 85, "Aayush": 92, "Bikash": 78}
+scores = {"Rushan": 85, "Aayush": 82, "Bikash": 78}
 
 # By value
 sorted_by_value = dict(sorted(scores.items(), key=lambda x: x[1]))
